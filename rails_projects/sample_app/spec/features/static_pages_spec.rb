209259a649c6302b9fd54dcdbbc2bff90203db19
Naveen Krishnan <naveen@payoff.com>
2014-07-31 20:27:13 -0700

    Finish static pages

--- rails_projects/sample_app/spec/features/static_pages_spec.rb
+++ rails_projects/sample_app/spec/features/static_pages_spec.rb
@@ -8,27 +8,42 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-
+=begin
     it "should have title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Ruby on Rails Tutorial sample | Home')
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Home')
     end
+=end
   end
 
      describe "Help page" do
     
-      it "should have content Help" do
+      it "should have the h1 Help" do
         visit '/static_pages/help'
-        page.should have_content('Help')
+        page.should have_selector('h1', :text => 'Help')
       end
+=begin
+    it "should have title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Help')
     end
+=end
+  end
 
-     describe "About Help" do
+     describe "About page" do
   
-       it "should have content about Help" do
+       it "should have the h1 'About Us'" do
           visit '/static_pages/about'
-          page.should have_content('About Us')
+          page.should have_selector('h1', :text => 'About Us')
        end
+=begin    
+     it "should have title 'About us'" do
+        visit '/static_pages/about'
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
     end
-end
+=end
+   end
+
+   end
+
 
